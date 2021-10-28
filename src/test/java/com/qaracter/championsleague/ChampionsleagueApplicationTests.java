175ba37b9ff87813c20f2cdbd54a833a0adbf446
Daniel Fraga <daniel.fraga@qaracter.es>
2021-10-28 14:41:17 +0200

    spring project configuration

---
+++ src/test/java/com/qaracter/championsleague/ChampionsleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qaracter.championsleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionsleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
